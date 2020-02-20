1bb2ccd297db7da9b72dd3f80e7592067fa368a4
RoanKathrina <roan.j.dimaculangan@accenture.com>
2020-02-20 10:59:48 +0800

    Updated to load three functionalities in one page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,12 @@ import {RouterModule, Routes} from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CustomerPageComponent } from './customer-page/customer-page.component';
 import { SignupComponent } from './signup/signup.component';
-import { CustomerPageListViewComponent } from './customer-page-list-view/customer-page-list-view.component';
-import { NewCustomerPageComponent } from './new-customer-page/new-customer-page.component'
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'customer-page', component: CustomerPageComponent},
   {path: 'signup', component: SignupComponent},
-  {path: 'customer-page-list-view', component: CustomerPageListViewComponent},
-  {path: 'new-customer-page', component: NewCustomerPageComponent}
 ];
 
 @NgModule({
--- src/app/customer-page/customer-page.component.ts
+++ src/app/customer-page/customer-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 import customers from '../../json/customers.json';
 
@@ -11,6 +12,9 @@ import customers from '../../json/customers.json';
 export class CustomerPageComponent implements OnInit {
 
   customers = customers;
+  containerToBeLoaded: string;
+  newCustomerForm: FormGroup;
+  newCustomerFormSubmitted: boolean = false;
 
   constructor(private router: Router,
               private route: ActivatedRoute) { }
@@ -23,9 +27,61 @@ export class CustomerPageComponent implements OnInit {
     else {
       this.customers = JSON.parse(window.sessionStorage.getItem('customers'));
     }
+
+    this.containerToBeLoaded = 'cardView';
+    this.initForm();
   }
 
   goToCustomersPage() {
     this.router.navigate(['../customer-page'], {relativeTo: this.route})
   }
+
+  setContainerToBeLoaded(container: string) {
+    this.containerToBeLoaded = container;
+  }
+
+  initForm() {
+    this.newCustomerForm = new FormGroup({
+      'firstName': new FormControl(null, Validators.required),
+      'lastName': new FormControl(null, Validators.required),
+      'address': new FormControl(null, Validators.required),
+      'gender': new FormControl('', Validators.required)
+    })
+  }
+
+  onSubmit() {
+    this.newCustomerFormSubmitted = true;
+
+    if(this.newCustomerForm.invalid === false) {
+      const firstName = this.newCustomerForm.get('firstName').value;
+      const lastName = this.newCustomerForm.get('lastName').value;
+      const customerFlg = this.customers.customers.find(item => item.first_name === firstName && item.last_name === lastName);
+
+      if(customerFlg !== undefined) {
+        window.alert('Error: Customer is already added in the Customer Manager Tool.');
+        return;
+      }
+      else {
+        const customersJSON = {
+          "first_name": firstName,
+          "last_name": lastName,
+          "address": this.newCustomerForm.get('address').value,
+          "gender": this.newCustomerForm.get('gender').value
+        }
+
+        this.customers.customers.push(customersJSON);
+        window.sessionStorage.setItem('customers', JSON.stringify(this.customers));
+        this.newCustomerForm.reset();
+        this.newCustomerFormSubmitted = false;
+        this.containerToBeLoaded = 'cardView';
+      }
+    }
+
+    else {
+      return;
+    }
+  }
+
+
+
 }
